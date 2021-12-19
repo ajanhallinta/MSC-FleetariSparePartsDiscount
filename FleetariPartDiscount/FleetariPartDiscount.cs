b9de595dbb05a09fd3bac61dc4607143cee48637
ajanhallinta <4janhallinta@gmail.com>
2021-12-20 00:38:12 +0200

    initial upload

---
+++ FleetariPartDiscount/FleetariPartDiscount.cs
@@ -0,0 +1,46 @@
+﻿using MSCLoader;
+using UnityEngine;
+
+namespace FleetariPartDiscount
+{
+    public class FleetariPartDiscount : Mod
+    {
+        public override string ID => "FleetariPartDiscount"; //Your mod ID (unique)
+        public override string Name => "Fleetari Parts Discount"; //You mod name
+        public override string Author => "ajanhallinta"; //Your Username
+        public override string Version => "1.0"; //Version
+        public override string Description => "Apply Fleetari's discount to spare parts."; //Short description of your mod
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.OnLoad, Mod_OnLoad);
+        }
+
+        public override void ModSettings()
+        {
+            // All settings should be created here. 
+            // DO NOT put anything else here that settings or keybinds
+            Settings.AddHeader(this, "Support");
+            Settings.AddText(this, "Please... I'm begging you...");
+            Settings.AddButton(this, "support", "PayPal", () =>
+            {
+                try
+                {
+                    Application.OpenURL("https://paypal.me/ajanhallinta");
+                }
+                catch
+                {
+                }
+            });
+        }
+
+        private void Mod_OnLoad()
+        {
+            // Called once, when mod is loading after game is fully loaded
+            GameObject go = new GameObject();
+            go.name = "Spare Parts Discount";
+            go.transform.parent = GameObject.Find("REPAIRSHOP").transform.GetChild(7).GetChild(9).GetChild(3);
+            go.AddComponent<PartsDiscount>();
+        }
+    }
+}
---
+++ FleetariPartDiscount/PartsDiscount.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+namespace FleetariPartDiscount
+{
+    public class PartsDiscount : MonoBehaviour
+    {
+        private static void DebugPrint(string msg)
+        {
+            MSCLoader.ModConsole.Print("Discount: " + msg);
+        }
+
+        private void OnEnable()
+        {
+            DebugPrint("Enabled");
+            UpdateDiscount();
+        }
+
+        public void UpdateDiscount()
+        {
+            PlayMakerFSM discountFsm = null;
+            float discount = -1;
+
+            try
+            {
+                discountFsm = Resources.FindObjectsOfTypeAll<PlayMakerFSM>().Where(x => x.name == "PriceDiscount").FirstOrDefault();
+            }
+            catch
+            {
+                DebugPrint("no discountFsm");
+                return;
+            }
+
+            try
+            {
+                discount = MSCLoader.PlayMakerExtensions.GetVariable<FsmFloat>(discountFsm, "DiscountPercent").Value;
+            }
+            catch
+            {
+                DebugPrint("error when trying to get discount percent");
+            }
+
+            if (discount == -1)
+            {
+                DebugPrint("no discount price float value");
+                return;
+            }
+
+            // update part price in fsm variables
+            foreach (PlayMakerFSM partFsm in transform.parent.GetComponentsInChildren<PlayMakerFSM>())
+            {
+                // create custom fsmfloat for original price
+                FsmFloat ogPrice = new FsmFloat();
+                ogPrice.Name = "OriginalPrice";
+                ogPrice.Value = partFsm.FsmVariables.GetFsmFloat("Price").Value;
+                MSCLoader.PlayMakerExtensions.AddVariable(partFsm, ogPrice);
+
+                float ogPriceValue = partFsm.FsmVariables.GetFsmFloat("Price").Value;
+
+                // calculate price from og price
+                float newPrice = (int)(ogPriceValue - (ogPriceValue * discount));
+                if (newPrice == 0)
+                    continue;
+
+                partFsm.FsmVariables.GetFsmFloat("Price").Value = newPrice;
+
+                // get and split price string
+                FsmString notification = partFsm.FsmVariables.GetFsmString("Notification");
+                string[] priceStrings = notification.Value.Split(' ');
+                priceStrings[priceStrings.Length - 2] = newPrice.ToString();
+
+                // create new price string
+                string newPriceString = "";
+                for (int i = 0; i < priceStrings.Length; i++)
+                {
+                    newPriceString += priceStrings[i];
+                    if (i != priceStrings.Length - 1)
+                        newPriceString += " ";
+                }
+
+                // apply new price string
+                notification.Value = newPriceString;
+            }
+
+            DebugPrint("Applied Parts Discount, destroying...");
+            Destroy(gameObject);
+        }
+    }
+}
